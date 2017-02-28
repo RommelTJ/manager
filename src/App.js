ca5820c9481ddf6aa49af1ba08e5f175cd2665db
Rommel Rico <me@rommelrico.com>
2017-02-27 20:43:09 -0800

    Boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+	componentWillMount() {
+		const config = {
+    		apiKey: 'AIzaSyA8J6UjrnuvZjkcuj7M7SAINB_BFUkFv-E',
+    		authDomain: 'manager-dd564.firebaseapp.com',
+    		databaseURL: 'https://manager-dd564.firebaseio.com',
+    		storageBucket: 'manager-dd564.appspot.com',
+    		messagingSenderId: '243930711501'
+  		};
+  		
+  		firebase.initializeApp(config);
+	}
+
+	render() {
+		return (
+			<Provider store={createStore(reducers)} >
+				<View>
+					<Text>
+						Hello!
+					</Text>
+				</View>
+			</Provider>
+		);
+	}
+}
+
+export default App;
